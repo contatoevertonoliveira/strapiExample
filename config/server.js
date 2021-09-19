961c4fe3a7611145442be33eca729238c57ca950
Everton Oliveira <contato.evertonoliveira@hotmail.com>
2021-09-19 20:29:07 -0300

    Strapi Settings

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4a3824b35fa2827467f47b751715b96a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '77f78ae6-0f0a-4510-b126-1aa891899eb5'
+};
